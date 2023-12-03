8302a71bf7a901196bf8fb6c7d117c596c146985
Ali Soliman <alisolimanworks@gmail.com>
2023-12-03 17:13:38 +0200

    Include AllTasks Component

--- src/layout/main-layout/Screens/task-details/index.tsx
+++ src/layout/main-layout/Screens/task-details/index.tsx
@@ -2,6 +2,7 @@ import { Stack } from "@mui/material";
 import TitleAndDate from "./TitleAndDate";
 import Details from "./Details";
 import Progress from "./Progress";
+import AllTasks from "./All Tasks";
 
 function TaskDetails() {
   return (
@@ -9,6 +10,7 @@ function TaskDetails() {
       <TitleAndDate />
       <Details title={DETAILS_TITLE} description={DESCRIPTION} />
       <Progress title={PROGRESS_TITLE} progress={70} />
+      <AllTasks title={TASKS_TITLE} tasks={TASKS} />
     </Stack>
   );
 }
@@ -16,5 +18,14 @@ function TaskDetails() {
 const DETAILS_TITLE = "Project Details",
   DESCRIPTION =
     "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled",
-  PROGRESS_TITLE = "Project Progress";
+  PROGRESS_TITLE = "Project Progress",
+  TASKS_TITLE = "Tasks",
+  TASKS: { name: string; done?: boolean }[] = [
+    { name: "User Interviews", done: true },
+    { name: "Wireframes", done: true },
+    { name: "Design System", done: true },
+    { name: "Icons", done: true },
+    { name: "Final Mockups", done: false },
+    { name: "Real Estate App Design", done: false },
+  ];
 export default TaskDetails;
